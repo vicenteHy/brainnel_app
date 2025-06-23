33a8b991877da4bf2f436843c90b9a91a83dc8d3
vicentehy <k99qsm533@gmail.com>
2025-06-24 01:44:16 +0800

    上架前

--- app/types/react-native-md5.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'react-native-md5' {
-  export function md5(str: string): string;
-} 
--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 import { registerRootComponent } from 'expo';
 
+// 添加 polyfill 来解决 NativeModule undefined 问题
+if (typeof global !== 'undefined') {
+  // 确保 global.__fbBatchedBridge 存在
+  if (!global.__fbBatchedBridge) {
+    global.__fbBatchedBridge = {
+      callFunctionReturnFlushedQueue: () => [],
+      invokeCallbackAndReturnFlushedQueue: () => [],
+      flushedQueue: () => [],
+      callFunctionReturnResultAndFlushedQueue: () => [],
+    };
+  }
+  
+  // 确保 global.nativeExtensions 存在
+  if (!global.nativeExtensions) {
+    global.nativeExtensions = {};
+  }
+}
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
